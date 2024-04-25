7910b9be906106f834aadd7945a020ae8ddf8e70
Mana <57663038+Manaball123@users.noreply.github.com>
2024-04-25 22:22:21 +0800

    Update main.cpp

--- aes-guesser/main.cpp
+++ aes-guesser/main.cpp
@@ -26,9 +26,7 @@ int main() {
 	AES aes(AESKeyLength::AES_256);
 	//						      49 6d 4e 6f 74 47 61 79 49 4a 75 73 74 48 61 76 65 41 50 65 6e 69 73 46 65 74 69 73 68 3a 33 00
 	std::string wildcarded_key = "49 6d 4e 6f 74 47 61 79 49 4a 75 ?? 74 48 61 76 65 41 50 65 6e 69 73 46 65 ?? 69 73 68 3a 33 00";
-	std::cout << GetEntropy(buf, sizeof(buf_static)) << std::endl;
 	auto c = aes.EncryptECB(buf, 256, key);
-	auto d = aes.DecryptECB(c, 256, key);
 	AESGuesser guesser(wildcarded_key, c, 256);
 	auto res = guesser.BruteforceKey();
 	for (auto& r : res) {
